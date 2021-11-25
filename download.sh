d57baa8805cbd061896ce3b9fd5dbbedfe86baa6
Angie <19460541@student.curtin.edu.au>
2021-11-25 09:05:34 +0000

    Debugging

--- download.sh
+++ download.sh
@@ -1,11 +1,12 @@
-set -euxo pipefail
-index=$1
-obsid=$2
+obsid=$1
+index=$2
+echo ${obsid}
 if giant-squid submit-conv ${obsid} --wait -p timeres=0.5,freqres=160,edgewidth=160; then
-	if ssh garrawarla "/astro/mwasci/awaszewski/project/start.sh ${obsid}"; then
+	if ssh garrawarla "bash /astro/mwasci/awaszewski/project/start.sh ${obsid}"; then
 		echo ${obsid} ${index} >> complete.txt
 	else
 		echo ${obsid} ${index} >> ssh_failed.txt
+	fi
 else
 	echo ${obsid} ${index} >> giant_squid_failed.txt
 fi
