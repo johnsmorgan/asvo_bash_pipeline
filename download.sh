5a38c1e22f2fa3b6ae9f95359364ad97a78ddfd4
Angie <19460541@student.curtin.edu.au>
2021-12-08 03:01:46 +0000

    Include initialising and accessing database

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@ index=$2
 echo ${obsid}
 if giant-squid submit-conv ${obsid} --wait -p timeres=0.5,freqres=160,edgewidth=160; then
 	if ssh garrawarla "bash /astro/mwasci/awaszewski/project/start.sh ${obsid}"; then
-		echo ${obsid} ${index} >> complete.txt
+		echo ${obsid} ${index} >> ssh_succeed.txt
 	else
 		echo ${obsid} ${index} >> ssh_failed.txt
 	fi
