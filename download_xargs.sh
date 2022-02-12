4c54b3a92e41c68c0c54de0840c1748b753ad72d
angie-wasz <19460541@student.curtin.edu.au>
2022-02-12 13:48:15 +0000

    Clearing not needed scripts

--- download_xargs.sh
+++
@@ -1,8 +0,0 @@
-set -euxo pipefail
-
-#Initialise database where completion information will be stored
-ssh garrawarla "cd /astro/mwasci/awaszewski/project; export DB_FILE=/astro/mwasci/awaszewski/project/log.sqlite; python3 init_db.py"
-
-#Run through all obsids, running 10 at any one time
-n_parallel=10
-cat obsids_test | xargs -P $n_parallel -d $'\n' -n 1 ./download.sh
